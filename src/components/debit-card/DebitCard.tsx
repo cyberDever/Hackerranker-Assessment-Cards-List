1c71e8cc737d874bc117fedf212f3938aa19dda0
donpushme <midabrey@gmail.com>
2025-11-20 14:45:58 +0900

    CardHolder Finished

--- src/components/debit-card/DebitCard.tsx
+++ src/components/debit-card/DebitCard.tsx
@@ -2,68 +2,72 @@ import React, { useState } from "react";
 
 import "./DebitCard.css";
 import { cards } from "../../cards";
-import type { Card } from "../../cards"
+import type { Card } from "../../cards";
 
 export const DebitCard: React.FC = () => {
 
-
-	const cardList: Card[] = { ...cards };
-	const [card, setCard] = useState({
-		number: "",
-		expiry: "",
-		cvv: "",
-		name: "",
-		bank: "",
-	});
-	const onSelectCard = (e: number) => {
-		const cardId = e;
-		setCard(cards[cardId]);
+	const [isButtonClicked, setIsButtonClicked] = useState(false);
+	const [isCardClicked, setIsCardClicked] = useState(false);
+	const [cardId, setCardId] = useState(0);
+	
+	function stringFormat(str: string) {
+		let strr: string = "";
+		for (let i = 4; i <= str.length; i += 4) {
+			i === 4 ?
+				strr += str.slice(0, 4) :
+				strr += " " + str.slice(i - 4, i);
+		}
+		return strr;
 	}
-
-	console.log(cardList)
-
-
+	
+	function formatCardDetails(card: Card) {
+		let formattedCard: Card = {
+			...card,
+			number: card.number.slice(0, 4) + " XXXX XXXX XXXX",
+			name: "XXXX XXXX",
+			expiry: "XX/XX",
+			cvv: "XXX"
+		};
+		return formattedCard;
+	}
+	
+	let showDetails = !isCardClicked ? formatCardDetails(cards[cardId]) : {...cards[cardId], number: stringFormat(cards[cardId].number)};
+	
 	return (
 		<div className="mt-50 layout-column justify-content-center align-items-center" >
 			<div className="card outlined" style={{ width: '1000px' }}>
-				<div data-testid="debit-card">
+				{ isButtonClicked ? (<div data-testid="debit-card" onClick={() => {
+					setIsCardClicked(!isCardClicked);
+				}}>
 					<h3 style={{ textAlign: 'center' }}>Card Details</h3>
 					<br />
 					<div className="debit-card-body" data-testid="debit-card-body"
 					>
-						<p className="debit-card-bank" data-testid="debit-card-bank-name">{card.bank}</p>
-						<p className="debit-card-no" data-testid="debit-card-no">{card.number}</p>
+						<p className="debit-card-bank" data-testid="debit-card-bank-name">{showDetails.bank}</p>
+						<p className="debit-card-no" data-testid="debit-card-no">{showDetails.number}</p>
 						<br />
 						<div style={{ height: '45px', backgroundColor: 'black' }} className="debit-card-stripe"></div>
 						<p>
-							<span className="debit-card-holder-name" data-testid="debit-card-holder-name">{card.name}</span>
-							<span className="debit-card-date" data-testid="debit-card-expiry-date">{card.expiry}</span>
-							<span className="debit-card-cvv" data-testid="debit-card-cvv">{card.cvv}</span></p>
+							<span className="debit-card-holder-name" data-testid="debit-card-holder-name">{showDetails.name}</span>
+							<span className="debit-card-date" data-testid="debit-card-expiry-date">{showDetails.expiry}</span>
+							<span className="debit-card-cvv" data-testid="debit-card-cvv">{showDetails.cvv}</span></p>
 					</div>
-				</div>
+				</div>) : (<></>) }
 				<div>
 					<h3 style={{ textAlign: "center" }}>Cards List</h3>
 					<div className="debit-card-list" data-testid="debit-card-list">
-						{/* {
-							cardList.map((card, index) =>
-							(<div className="list-card" data-testid="list-card-2"
-								onClick={() => onSelectCard(index + 1)}
-							><p className="list-card-title">Card {index + 1}</p></div>))
-						} */}
-						<div className="list-card" data-testid="list-card-0"
-							onClick={() => onSelectCard(1)}>
-							<p className="list-card-title">Card 1</p>
-						</div>
-						<div className="list-card" data-testid="list-card-2"
-							onClick={() => onSelectCard(2)}
-						><p className="list-card-title">Card 2</p></div>
-						<div className="list-card" data-testid="list-card-3"
-							onClick={() => onSelectCard(3)} ><p className="list-card-title">Card 3</p></div>
-						<div className="list-card" data-testid="list-card-4"
-							onClick={() => onSelectCard(4)}><p className="list-card-title">Card 4</p></div>
-						<div className="list-card" data-testid="list-card-4"
-							onClick={() => onSelectCard(5)}><p className="list-card-title">Card 5</p></div>
-
+						{cards.map((card: Card, index: number) => {
+							return (
+								<div key={index} className="list-card" data-testid={`list-card-${index}`}
+									onClick={() => {
+										setIsButtonClicked(true);
+										setCardId(index);
+										setIsCardClicked(false);
+									}}>
+									<p className="list-card-title">Card {index + 1}</p>
+								</div>
+							);
+						})}
 					</div>
 				</div>
 			</div>
